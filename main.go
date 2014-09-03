0e51f38e696bb6e096461bed071c3333e7bc1ebc
Javed Khan <tuxcanfly@gmail.com>
2014-09-03 16:56:48 +0530

    split launch into a func

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"strings"
 	"sync"
 	"time"
 )
@@ -27,26 +28,52 @@ func init() {
 
 func main() {
 	var wg sync.WaitGroup
+
+	// filter out flags from args
+	// any leading arg starting with "-" is considered a flag
+	i := 1
+	flags := os.Args[1:]
+	for _, f := range flags {
+		if strings.HasPrefix(f, "-") {
+			i++
+		} else {
+			break
+		}
+	}
+	cmd := os.Args[i]
+	args := os.Args[i+1:]
+
 	for i := 0; i < *times; i++ {
 		wg.Add(1)
-		go func(i int, wg *sync.WaitGroup) {
-			cmd := exec.Command(os.Args[1], os.Args[2:]...)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			if err := cmd.Start(); err != nil {
-				log.Fatalf("%v", err)
-			}
-			wait := rand.Int()%(*maxwait-*minwait+1) + *minwait
-			time.AfterFunc(time.Duration(wait)*time.Second, func() {
-				if err := cmd.Process.Signal(os.Interrupt); err != nil {
-					log.Fatalf("%v", err)
-				}
-			})
-			if err := cmd.Wait(); err != nil {
-				log.Printf("process %v with timeout %v: %v", i, wait, err)
-			}
-			wg.Done()
-		}(i, &wg)
+		go Launch(cmd, args, i, &wg)
 	}
 	wg.Wait()
+}
+
+// Launch starts the passed exec cmd, sets a random timeout
+// to interrupt and waits for the process to finish
+func Launch(cmd string, args []string, i int, wg *sync.WaitGroup) {
+	c := exec.Command(cmd, args...)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	if err := c.Start(); err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	// generate random time within minwait to maxwait
+	wait := rand.Int()%(*maxwait-*minwait+1) + *minwait
+	time.AfterFunc(time.Duration(wait)*time.Second, func() {
+		// after random waiting time, send a SIGINT
+		if err := c.Process.Signal(os.Interrupt); err != nil {
+			log.Printf("%v", err)
+		}
+	})
+
+	// wait for the process to finish
+	// if the process has finished before timeout, allow this
+	// goroutine to exit
+	if err := c.Wait(); err != nil {
+		log.Printf("process %v with timeout %v: %v", i, wait, err)
+	}
+	wg.Done()
 }
